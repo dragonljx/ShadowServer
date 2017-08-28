436d3b07815ac250a387fc7a6024fcbb5df310cc
青 冥 <dragonljx@126.com>
2017-08-28 14:34:29 +0800

    x

---
+++ Assets/_Scripts/InfraredSourceView.cs
@@ -0,0 +1,117 @@
+﻿using UnityEngine;
+using Windows.Kinect;
+
+public class InfraredSourceView : MonoBehaviour
+{
+    private static InfraredSourceView _instance;
+    private KinectSensor _Sensor;
+
+    private InfraredFrameReader _Reader;
+    public ushort[] _Data;
+
+    private byte[] _RawData;
+    private Texture2D _Texture;
+    public static InfraredSourceView Instance { get { return _instance; } }
+
+    public Texture2D GetInfraredTexture()
+    {
+        return _Texture;
+    }
+
+    private void Start()
+    {
+        _instance = this;
+
+        _Sensor = KinectSensor.GetDefault();
+
+        if (_Sensor != null)
+        {
+            _Reader = _Sensor.InfraredFrameSource.OpenReader();
+            var frameDesc = _Sensor.InfraredFrameSource.FrameDescription;
+            _Data = new ushort[frameDesc.LengthInPixels];
+            _RawData = new byte[frameDesc.LengthInPixels * 4];
+            _Texture = new Texture2D(frameDesc.Width, frameDesc.Height, TextureFormat.BGRA32, false);
+
+            if (!_Sensor.IsOpen)
+            {
+                _Sensor.Open();
+            }
+        }
+
+        gameObject.GetComponent<Renderer>().material.SetTextureScale("_MainTex", new Vector2(-1, 1));
+    }
+
+    public void View(ushort[] data)
+    {
+        if (data != null)
+        {
+            int index = 0;
+            foreach (var ir in data)
+            {
+                byte intensity = (byte)(ir >> 8);
+                _RawData[index++] = intensity;
+                _RawData[index++] = intensity;
+                _RawData[index++] = intensity;
+                _RawData[index++] = 255; // Alpha
+            }
+
+            _Texture.LoadRawTextureData(_RawData);
+            _Texture.Apply();
+        }
+
+        gameObject.GetComponent<Renderer>().material.mainTexture = GetInfraredTexture();
+    }
+
+    private void Update()
+    {
+        #region 调用kinect 查看红外图
+
+        /*
+           if (_Reader != null)
+           {
+               var frame = _Reader.AcquireLatestFrame();
+               if (frame != null)
+               {
+                   frame.CopyFrameDataToArray(_Data);
+
+                   int index = 0;
+                   foreach (var ir in _Data)
+                   {
+                       byte intensity = (byte)(ir >> 8);
+                       _RawData[index++] = intensity;
+                       _RawData[index++] = intensity;
+                       _RawData[index++] = intensity;
+                       _RawData[index++] = 255; // Alpha
+                   }
+
+                   _Texture.LoadRawTextureData(_RawData);
+                   _Texture.Apply();
+
+                   frame.Dispose();
+                   frame = null;
+               }
+           }
+
+       */
+
+        #endregion 调用kinect 查看红外图
+
+        if (_Data != null)
+        {
+            int index = 0;
+            foreach (var ir in _Data)
+            {
+                byte intensity = (byte)(ir >> 8);
+                _RawData[index++] = intensity;
+                _RawData[index++] = intensity;
+                _RawData[index++] = intensity;
+                _RawData[index++] = 255; // Alpha
+            }
+
+            _Texture.LoadRawTextureData(_RawData);
+            _Texture.Apply();
+        }
+
+        gameObject.GetComponent<Renderer>().material.mainTexture = GetInfraredTexture();
+    }
+}
---
+++ Assets/_Scripts/TreatingData.cs
@@ -0,0 +1,124 @@
+﻿using UnityEngine;
+using Windows.Kinect;
+
+public class TreatingData : MonoBehaviour
+{
+    private static TreatingData _instance;
+    KinectSensor _Sensor;
+
+    private int resolutionDepth;
+    private ushort[] depthData;
+    private int verticesNumber = 60000;
+    private int verticesRemaining;
+    private Vector3[] coordinatesAll;
+    private Mesh[] meshAll;
+    private GameObject[] objAll;
+    private MeshFilter[] filterAll;
+    public Material mater;
+
+    public static TreatingData Instance { get { return _instance; } }
+    private void Start()
+    {
+        _instance = this;
+        _Sensor = KinectSensor.GetDefault();
+        if (!_Sensor.IsOpen)
+        {
+            _Sensor.Open();
+        }
+        resolutionDepth = 512 * 424;
+        verticesRemaining = resolutionDepth % verticesNumber;
+
+        coordinatesAll = new Vector3[resolutionDepth];
+        meshAll = new Mesh[4];
+        objAll = new GameObject[4];
+        filterAll = new MeshFilter[4];
+        for (int i = 0; i < 4; i++)
+        {
+            objAll[i] = new GameObject();
+            objAll[i].name = i.ToString();
+            filterAll[i] = objAll[i].AddComponent<MeshFilter>();
+            objAll[i].AddComponent<MeshRenderer>().material = mater;
+            meshAll[i] = new Mesh();
+        }
+
+
+    }
+
+    
+    /// <summary>
+    /// 深度图坐标转换
+    /// </summary>
+    public void TransCoordinate(ushort[] depth)
+    {
+        if (depth == null)
+        {
+            return;
+        }
+        CameraSpacePoint[] camSpace = new CameraSpacePoint[depth.Length];
+        _Sensor.CoordinateMapper.MapDepthFrameToCameraSpace(depth, camSpace);
+
+        for (int i = 0; i < resolutionDepth; i++)
+        {
+            if (float.IsInfinity(camSpace[i].X) || float.IsInfinity(camSpace[i].Y) || float.IsInfinity(camSpace[i].Z))
+            {
+                coordinatesAll[i] = Vector3.zero;
+                continue;
+            }
+
+            coordinatesAll[i].x = -camSpace[i].X;
+            coordinatesAll[i].y = camSpace[i].Y;
+            coordinatesAll[i].z = camSpace[i].Z;
+        }
+        for (int i = 0; i < 4; i++)
+        {
+            if (i == 4 - 1)
+            {
+                CloudReverse(meshAll[i], coordinatesAll, i * verticesNumber, verticesRemaining,i);
+                //filterAll[i].mesh = meshAll[i];
+                break;
+            }
+            CloudReverse(meshAll[i], coordinatesAll, i * verticesNumber, verticesNumber,i);
+            //filterAll[i].mesh = meshAll[i];
+
+        }
+    }
+    /// <summary>
+    /// 点云模型生成
+    /// </summary>
+    public void CloudReverse(Mesh mesh, Vector3[] vertices, int beginIndex, int pointsNum,int meshNum)
+    {
+        //异步执行以下内容
+        Loom.RunAsync(() => {
+        Vector3[] points = new Vector3[pointsNum];
+        int[] indecies = new int[pointsNum];
+            for (int i = 0; i < pointsNum; i++)
+            {
+                points[i] = vertices[beginIndex + i];
+                indecies[i] = i;
+            }
+            //调用loom在update中执行下列方法，达到主线程执行的功能
+            Loom.QueueOnMainThread(() => {
+                mesh.Clear();
+                mesh.vertices = points;
+                mesh.SetIndices(indecies, MeshTopology.Points, 0);
+                filterAll[meshNum].mesh = mesh;
+            });
+        });
+
+
+    }
+
+    public void SetUpServer(string ip, int port)
+    {
+        ReceiveCallBack back = delegate (ushort[] data)
+        {
+            TransCoordinate(data);
+            Debug.Log("回调");
+        };
+
+        Server.Instance.ServerInit(back, ip, port);
+    }
+
+
+
+}
